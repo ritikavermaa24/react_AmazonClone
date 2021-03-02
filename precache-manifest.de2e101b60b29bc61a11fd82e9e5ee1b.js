9a1a82667adbd9bfb14c4708eebc3ceac0313ca0
ritikavermaa24 <ritikav8898@gmail.com>
2021-03-02 16:10:10 +0530

    Updates

---
+++ precache-manifest.de2e101b60b29bc61a11fd82e9e5ee1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613cb3ccb0f5f39333053dd29e4973b6",
+    "url": "/react_AmazonClone/index.html"
+  },
+  {
+    "revision": "0560b0b95d40e06be159",
+    "url": "/react_AmazonClone/static/css/main.46cd71e0.chunk.css"
+  },
+  {
+    "revision": "908eddc55a0091686b6d",
+    "url": "/react_AmazonClone/static/js/2.a4eaef22.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/react_AmazonClone/static/js/2.a4eaef22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0560b0b95d40e06be159",
+    "url": "/react_AmazonClone/static/js/main.7ea78577.chunk.js"
+  },
+  {
+    "revision": "c5d31f7887ab02a5b2c1",
+    "url": "/react_AmazonClone/static/js/runtime-main.de177bb0.js"
+  },
+  {
+    "revision": "07c9983399b6fe36c11b350f3f983df9",
+    "url": "/react_AmazonClone/static/media/home.07c99833.jpg"
+  },
+  {
+    "revision": "9bce223f065acdf6f1332e1c2a14bf5a",
+    "url": "/react_AmazonClone/static/media/product2.9bce223f.png"
+  }
+]);
